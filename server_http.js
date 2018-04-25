c18a55452702317c233d79a3189a1dc17a7de1e6
hodgoong <hodgoong@gmail.com>
2018-04-25 22:26:32 +0200

    http redirect enabled

--- server_http.js
+++ server_http.js
@@ -1,12 +1,14 @@
-// set up plain http server
-var http = express.createServer();
+const express = require('express');
+const app = express();
+const http_port = 4201;
 
 // set up a route to redirect http to https
-http.get('*', function(req, res) {  
+app.get('*', function(req, res) { 
     res.redirect('https://flipcard.io' + req.url);
 
     // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
     // res.redirect('https://example.com' + req.url);
 })
 
-http.listen(4201);
+app.listen(http_port);
+console.log('https Server started on port %d', http_port);
