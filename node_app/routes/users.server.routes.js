e7e016d4fa34141b0f0b901ccd7ae95a3643f7d6
hodgoong <hodgoong@gmail.com>
2018-05-01 21:48:44 +0200

    rollback to previous code

--- node_app/routes/users.server.routes.js
+++ node_app/routes/users.server.routes.js
@@ -7,37 +7,37 @@ module.exports = function(app) {
     app.route('/auth/signup')
         .post(users.signup);
     
-    app.post('/auth/signin', function(req, res, next){
-        passport.authenticate('local', function(err, user, info){
-            if(err){
-                console.log(err)
-                res.status(400).json({ 
-                    message: err.message
-                });
-            } 
-            const token = jwt.sign(req.body.username, config.sessionSecret);
-            console.log('authentication has been successful');
-            res.status(200).json({ 
-                jwt: token
-            });
-        })
-    })
-
-    // app.route('/auth/signin')
-    //     .post(passport.authenticate('local'), 
-    //         function(req, res) {
+    // app.post('/auth/signin', function(req, res, next){
+    //     passport.authenticate('local', function(err, user, info){
+    //         if(err){
     //             console.log(err)
-    //             if(err){
-    //                 res.status(400).json({ 
-    //                     message: err.message
-    //                 });
-    //             }
-    //             console.log('authentication has been successful');
-    //             const token = jwt.sign(req.body.username, config.sessionSecret);
-
-    //             res.status(200).json({ 
-    //                 jwt: token
+    //             res.status(400).json({ 
+    //                 message: err.message
     //             });
-    //         }
-    //     )
+    //         } 
+    //         const token = jwt.sign(req.body.username, config.sessionSecret);
+    //         console.log('authentication has been successful');
+    //         res.status(200).json({ 
+    //             jwt: token
+    //         });
+    //     })
+    // })
+
+    app.route('/auth/signin')
+        .post(passport.authenticate('local'), 
+            function(req, res) {
+                console.log(err)
+                if(err){
+                    res.status(400).json({ 
+                        message: err.message
+                    });
+                }
+                console.log('authentication has been successful');
+                const token = jwt.sign(req.body.username, config.sessionSecret);
+
+                res.status(200).json({ 
+                    jwt: token
+                });
+            }
+        )
 };
