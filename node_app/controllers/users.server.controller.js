a29ec4a19730ed5afe6ecdf0a18d0e3ad4af7f09
hodgoong <hodgoong@gmail.com>
2018-05-01 23:01:33 +0200

    changed status 500 to 200 for the duplicated id

--- node_app/controllers/users.server.controller.js
+++ node_app/controllers/users.server.controller.js
@@ -38,8 +38,8 @@ exports.signup = function (req, res, next) {
             if (err) {
                 const message = getErrorMessage(err);
                 console.log(message);
-                console.log('sending 500')
-                res.status(500).json({ message: "ID is duplicated. Please use another ID." });
+                console.log('sending 200 to trigger the alert message in the frontend')
+                res.status(200).json({ message: "ID is duplicated. Please use another ID." });
             } else {
                 console.log('saving done');
                 res.status(200).json({ message: "New id has been created. Please sign in." });
